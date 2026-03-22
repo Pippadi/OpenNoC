877f77cf922d6289c1cbc0c7afd42a1698b2bf64
Prithvi Vishak <prithvivishak@gmail.com>
2026-03-22 11:51:10 +0530

    Increment segment counter and fix PE allocation map

--- src/Verilog/tb/conv_tb_noc.v
+++ src/Verilog/tb/conv_tb_noc.v
@@ -22,7 +22,7 @@ module conv_tb_noc();
 reg clk;
 reg rst_n;
 reg [`PIX_WIDTH-1:0] imgData;
-integer file, file1, i;
+integer file, file1, i, j;
 
 localparam SEG_WIDTH = `IMG_WIDTH / `SEG_CNT_X;
 localparam SEG_HEIGHT = `IMG_HEIGHT / `SEG_CNT_Y;
@@ -37,7 +37,7 @@ reg [`IMG_WIDTH*`PIX_WIDTH-1:0] img [0:`IMG_HEIGHT-1];
 // Most significant bit for idle, next bits for segment index, remaining bits for line index
 // Is there a better way to do this?
 localparam PE_MAP_WIDTH = 1 + $clog2(SEG_CNT_TOT) + $clog2(SEG_HEIGHT);
-reg [PE_MAP_WIDTH-1:0] pe_segment_map [0:$clog2(`NOC_X)-1][0:$clog2(`NOC_Y)-1];
+reg [PE_MAP_WIDTH-1:0] pe_segment_map [0:`NOC_X-1][0:`NOC_Y-1];
 
 initial begin
     clk = 1'b0;
@@ -45,14 +45,12 @@ initial begin
 end
 
 // Timeout for infinite loop and short simulation runs when using dumpvars
-/*
 initial begin
-    #100000;
+    #50000;
     $fclose(file);
     $fclose(file1);
     $finish;
 end
-*/
 
 // segment_line extracts the appropriate line segment with halo pixels for the given segment index.
 // It handles edge cases for halo pixels by zero-padding when out of bounds.
@@ -69,7 +67,9 @@ function automatic [LINE_WIDTH*`PIX_WIDTH-1:0] segment_line(input [`IMG_WIDTH*`P
 end
 endfunction
 
-reg [$clog2(SEG_CNT_TOT)-1:0] next_seg;
+// Make sure that it can reach SEG_CNT_TOT to end the simulation
+reg [$clog2(SEG_CNT_TOT):0] next_seg;
+
 reg [$clog2(`NOC_X)-1:0] pe_idx_x;
 reg [$clog2(`NOC_Y)-1:0] pe_idx_y;
 reg tx_chunk_ready;
@@ -99,16 +99,14 @@ reg [7:0] aByte;
 reg [7:0] line_temp [0:`IMG_WIDTH-1];
 initial begin
     // Uncomment for value change dump
-    /*
     $dumpfile("conv_tb_noc.vcd");
     $dumpvars(0, conv_tb_noc);
-    */
 
     //file = $fopen("../../../../../../../data/gray_512x512.bmp", "rb");
-    file = $fopen("../../../../../../../data/lena512.bmp", "rb");
-    file1 = $fopen("../../../../../../../data/outputLena.bmp", "wb");
-    //file = $fopen("../../../data/lena512.bmp","rb");       // Uncomment when
-    //file1 = $fopen("../../../data/outputLena.bmp","wb");   // using Icarus Verilog
+    //file = $fopen("../../../../../../../data/lena512.bmp", "rb");
+    //file1 = $fopen("../../../../../../../data/outputLena.bmp", "wb");
+    file = $fopen("../../../data/lena512.bmp","rb");       // Uncomment when
+    file1 = $fopen("../../../data/outputLena.bmp","wb");   // using Icarus Verilog
     for (i = 0; i < `BMP_HEADER_SIZE; i = i + 1) begin
         $fscanf(file, "%c", imgData);
         $fwrite(file1, "%c", imgData);
@@ -118,7 +116,7 @@ initial begin
     for (i = 0; i < `IMG_HEIGHT; i = i + 1) begin
         $fread(line_temp, file, 0, `IMG_WIDTH);
         //$display("%d", line_temp[511]);
-        for (integer j = 0; j < `IMG_WIDTH; j = j + 1) begin
+        for (j = 0; j < `IMG_WIDTH; j = j + 1) begin
             img[i][j*`PIX_WIDTH +: `PIX_WIDTH] = line_temp[j];
         end
 
@@ -129,13 +127,9 @@ initial begin
     rst_n = 1;
     #100;
 
-    for (i = 0; i < `NOC_X; i = i + 1)
-        for (integer j = 0; j < `NOC_Y; j = j + 1)
-            pe_segment_map[i][j] <= {PE_MAP_WIDTH{1'b0}};
-
     while (1) begin
         #5
-        if (pe_idx_x == `NOC_X-1 && pe_idx_y == `NOC_Y-1 && pe_segment_map[pe_idx_x][pe_idx_y][PE_MAP_WIDTH-1] == 0) begin
+        if (next_seg == SEG_CNT_TOT) begin
             // All segments sent and processed
             $fclose(file);
             $fclose(file1);
@@ -153,13 +147,18 @@ always @ (posedge clk) begin
         next_seg <= 0;
         pe_idx_x <= 0;
         pe_idx_y <= 1;
+        for (i = 0; i < `NOC_X; i = i + 1)
+            for (j = 0; j < `NOC_Y; j = j + 1)
+                pe_segment_map[i][j][PE_MAP_WIDTH-1] <= 1'b0;
+
     end else begin
         case (state)
         IDLE: begin
             // Cycle through PEs to find an idle one, assign next segment, and move to SEND state. If no idle PE, stay in IDLE and check again next cycle.
+            $display("PE %d, %d: %b", pe_idx_x, pe_idx_y, pe_segment_map[pe_idx_x][pe_idx_y]);
             if (pe_segment_map[pe_idx_x][pe_idx_y][PE_MAP_WIDTH-1] == 0) begin
                 // Mark PE as busy, assign segment index, and preserve line index (incremented when line received by PE)
-                pe_segment_map[pe_idx_x][pe_idx_y][PE_MAP_WIDTH-1] <= {1'b1, next_seg, pe_segment_map[pe_idx_x][pe_idx_y][$clog2(SEG_HEIGHT)-1:0]};
+                pe_segment_map[pe_idx_x][pe_idx_y] <= {1'b1, next_seg, pe_segment_map[pe_idx_x][pe_idx_y][$clog2(SEG_HEIGHT)-1:0]};
                 state <= SEND;
             end else begin
                 state <= IDLE;
@@ -172,7 +171,12 @@ always @ (posedge clk) begin
         end
 
         // line_chunker is active and sending chunks for the assigned segment. Once complete, return to IDLE state.
-        SEND: state <= tx_line_complete ? IDLE : SEND;
+        SEND: begin
+             if (tx_line_complete) begin
+                 state <= IDLE;
+                 next_seg <= next_seg + 1;
+             end
+        end
         endcase
     end
 end
