068ab7b59f49940158439153961905f7de648dc0
Ayush Yadav <ayush3105yadav@gmail.com>
2026-03-27 18:19:54 +0530

    implemented read logic

--- src/Verilog/rtl/convolution/noc_pe/reassembler.v
+++ src/Verilog/rtl/convolution/noc_pe/reassembler.v
@@ -26,18 +26,20 @@ module reassembler #(
     parameter IMAGE_HEIGHT = 512,
     parameter PIXEL_DEPTH = 8,
     parameter CHUNK_WIDTH = 2, // number of pixels sent by each chunk
-    parameter SEG_CNT_X = 2,
+    parameter SEG_CNT_X = 2,   // total number of segments in x and y direction
     parameter SEG_CNT_Y = 2,
     parameter MAX_CHUNK_CNT_X = 2, // within a segment
-    parameter MAX_CHUNK_CNT_Y = 2
+    parameter MAX_CHUNK_CNT_Y = 2,
+    parameter READ_PIXEL_CNT = 4 // number of pixels read out at a time, row-wise
 )(
-    input wire clk, rst, chunk_avail,
+    input wire clk, rst, chunk_avail, mode, // mode:0 for writing, 1 for reading
     input wire [CHUNK_WIDTH*PIXEL_DEPTH-1:0] chunk_in,
     input wire [$clog2(SEG_CNT_X)-1:0] seg_idx_x,
     input wire [$clog2(SEG_CNT_Y)-1:0] seg_idx_y,
     input wire [$clog2(MAX_CHUNK_CNT_X)-1:0] chunk_idx_x,
     input wire [$clog2(MAX_CHUNK_CNT_Y)-1:0] chunk_idx_y,
-    output wire opReady
+    output wire opReady,
+    output reg [READ_PIXEL_CNT*PIXEL_DEPTH-1:0] pixel_out
 );
     
     // STORES THE ENTIRE IMAGE
@@ -51,17 +53,31 @@ module reassembler #(
     
     assign opReady = &chunkValid;
     
+    // read index
+    reg [$clog2(IMAGE_HEIGHT*IMAGE_WIDTH/READ_PIXEL_CNT)-1:0] readIndex;
+
     integer i;
     always @ (posedge clk) begin
-        if (~rst) begin
-            chunkValid <= 0;
-        end else begin
-            if (chunk_avail) begin
-                chunkValid[chunkAddress] <= 1;
-         
-                for (i = 0; i < CHUNK_WIDTH; i = i + 1) begin
-                    imageOut[chunkAddress*CHUNK_WIDTH + i] <= chunk_in[i*PIXEL_DEPTH +: PIXEL_DEPTH];
+        if (~mode) begin
+            if (~rst) begin
+                chunkValid <= 0;
+            end else begin
+                if (chunk_avail) begin
+                    chunkValid[chunkAddress] <= 1;
+            
+                    for (i = 0; i < CHUNK_WIDTH; i = i + 1) begin
+                        imageOut[chunkAddress*CHUNK_WIDTH + i] <= chunk_in[i*PIXEL_DEPTH +: PIXEL_DEPTH];
+                    end
                 end
+            end
+        end else begin
+            if (~rst) begin
+                readIndex <= 0;
+            end else begin
+                for (i = 0; i < READ_PIXEL_CNT; i = i + 1) begin
+                    pixel_out[i*PIXEL_DEPTH +: PIXEL_DEPTH] <= imageOut[readIndex+i];
+                end
+                readIndex <= readIndex + READ_PIXEL_CNT;
             end
         end
     end
